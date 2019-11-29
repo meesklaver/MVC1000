9249d21cbc198d3679f8c876cc8ef0292e1b8c3c
meesklaver <33187828+meesklaver@users.noreply.github.com>
2019-11-29 11:29:45 +0100

    First Commit MVC1000
    Leeg MVC project

---
+++ MVC1000/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC1000.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
